30037d2070c52372a02d972612097fb3c2b2d69c
LuanaGD <l.trannguyen@rubika-edu.com>
2019-12-11 16:57:23 +0100

    Inté intégrale. les scripts sont à faire, ainsi que les animators

---
+++ LTN_Game&Watch/Assets/LTN_Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int [] MarioPos;
+    public int[] LuigiPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MarioMovement();
+    }
+
+    public void MarioMovement()
+    {
+        
+    }
+}
